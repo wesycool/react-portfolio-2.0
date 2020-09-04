60dd953b80f94dc99db58a6b32ea51258c5c7860
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-09-04 17:01:53 -0400

    Initial Files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const apiRouter = require('./app/router')
+const app = express()
+const orm = require('./app/orm.js');
+const mongoose = require( 'mongoose' );
+const API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080'
+
+const PORT = process.env.PORT || 8080
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio" , 
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+ // for serving all the normal html
+app.use( express.static('client/build') )
+// for parsing incoming POST data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+// for routes
+apiRouter(app)
+
+app.get('/*', function (req, res) {
+    console.log( `[/*] (${req.protocol}//${req.get('host')}/${req.originalUrl} -- sending file: ${__dirname}/client/build/index.html` );
+    res.sendFile(`${__dirname}/client/build/index.html`);
+ });
+ 
+
+app.listen(PORT, function() {
+    console.log( `Serving app at: http://localhost:${PORT}` )
+})
