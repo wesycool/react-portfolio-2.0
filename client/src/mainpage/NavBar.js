2168b08644c5f0edb6cfa21f0cdab5702ce02bb6
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-09-05 13:54:36 -0400

    Added NavBar items

--- client/src/mainpage/NavBar.js
+++ client/src/mainpage/NavBar.js
@@ -3,28 +3,41 @@ import { Link, useLocation } from "react-router-dom";
 
 function NavBar() {
     const {pathname} = useLocation()
+    const navItem = [
+        { 'name': 'HOME', 'link': '/' },
+        { 'name': 'SERVICE', 'link': '/service' },
+        { 'name': 'PORTFOLIO', 'link': '/portfolio' },
+        { 'name': 'BLOG', 'link': '/blog' },
+        { 'name': 'ABOUT', 'link': '/about' },
+        { 'name': 'CONTACT', 'link': '/contact' }
+    ]
 
     return (
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <div class="container-fluid">
-                <div class="row">
-                    <button class="navbar-toggler col-2 col-sm-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-                        <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark shadow">
+            <div className="container-fluid">
+                <div className="row">
+                    <button className="navbar-toggler col-2 col-sm-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class='col-8 col-sm-10 col-md-1 ml-md-2 ml-lg-5 navbar-brand'>
-                        <Link to="/" class="navbar-brand">E D M U N D</Link>
+                    <div className='col-8 col-sm-10 col-md-1 ml-md-2 ml-lg-5 navbar-brand'>
+                        <Link to="/" className="navbar-brand">E D M U N D</Link>
                     </div>
 
 
-                    <div class="collapse navbar-collapse col-12 col-md mr-md-2 mr-lg-5 justify-content-end" id="navbarSupportedContent">
-                        <ul class="navbar-nav mb-md-0">
-                            <li class="nav-item">
-                                <Link to="/about" class={pathname === '/about' ? "nav-link active" : "nav-link"}>ABOUT</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/contact" class={pathname === '/contact' ? "nav-link active" : "nav-link"}>CONTACT</Link>
-                            </li>
+                    <div className="collapse navbar-collapse col-12 col-md mr-md-2 mr-lg-5 justify-content-end" id="navbarSupportedContent">
+                        <ul className="navbar-nav mb-md-0 mt-2">
+
+                            {navItem.map(({name,link}) => {
+                                return(
+                                    <li className="nav-item">
+                                        <Link to={link} className={`nav-link ${pathname === link ? "active" : ""}`}>
+                                            {name}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+
                         </ul>
                     </div>
 
