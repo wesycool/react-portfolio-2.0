93e9dcfb8e409bf8022b7dd2a9b0f100ea3682a9
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-09-17 16:00:09 -0400

    Added projectBG image

--- client/src/mainpage/Project.js
+++ client/src/mainpage/Project.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext, useEffect} from "react"
-import {useLocation} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 import PortfolioContext from "../component/PortfolioContext"
+import './Project.css'
 
 function Project() {
     const {pathname} = useLocation()
@@ -16,10 +17,17 @@ function Project() {
 
     return(
         <div className='container'>
-            <div className='shadow-sm' style={{background:'url(../assets/img/portfolioBG.jpg) center', height:'150px', backgroundSize:'cover'}}>
-                <h1 className="dosis text-uppercase font-weight-bold text-center pt-5">{getProject.title}</h1>
-            </div>
+            <div className='shadow-sm projectBG' style={{backgroundImage:'url(https://www.splitpixel.co.uk/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/08/debtadviceheader1600-720x400.jpg.webp)'}}>
+                <div className='colorBG text-center lh-sm'>
+                    <div style={{color:'white', padding:'10%', paddingTop:'min(5vh,5vw)'}}>
+                        <h2 className="dosis text-uppercase font-weight-bold">{getProject.title}</h2>
+                        <hr className='bg-secondary' style={{height: '5px', opacity:'100%'}}/>
+                        <p style={{fontSize:'min(3.25vw,3.25vh, 20px)',paddingLeft:'3%', paddingRight:'3%'}}>{getProject.description}</p>
+                        <Link to={getProject.link} className="btn btn-secondary" style={{fontSize:'min(3.25vw,3.25vh, 15px)'}}>View Website</Link>
+                    </div>
+                </div>
 
+            </div>
         </div>
     )
 }
