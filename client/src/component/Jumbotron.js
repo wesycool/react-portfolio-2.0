296b4c34ad3d19bd1e30894ea788f101b823f7ce
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-10-29 13:46:03 -0400

    Update Jumbotron to support Safari orientation

--- client/src/component/Jumbotron.js
+++ client/src/component/Jumbotron.js
@@ -2,27 +2,28 @@ import React, {useState, useEffect} from "react"
 import { Link } from "react-router-dom";
 import './Jumbotron.css'
 
-function Jumbotron({info}){
+function Jumbotron({info:{title,p,h1,quotes,link,footer,btn}}){
     const [ background, setBackground ] = useState()
 
-    useEffect(() => {
-        const handleResize = () => {
-            const orientation = window.screen.orientation.type.split('-')
-            setBackground(`./assets/img/${info.title}BG-${orientation[0]}.jpg`)
-        }
-
-        handleResize()
-        window.addEventListener('resize', handleResize)
+    useEffect(()=>{
+        orientationChange()
+        window.addEventListener('resize', orientationChange)
     },[])
 
+    const orientationChange = () =>{
+        const {width,height} = window.screen
+        const orientation = width < height? 'portrait' : 'landscape'    
+        setBackground(`./assets/img/${title}BG-${orientation}.jpg`)
+    }
+
     return(
-        <div className={`${info.title}-bg jumbotron`} style={{backgroundImage:`url(${background})`}}>
+        <div className={`${title}-bg jumbotron`} style={{backgroundImage:`url(${background})`}}>
             <div className='container-fluid' style={{height: '100%'}}>
                 <blockquote className="ml-md-2 ml-lg-5">
-                    <h1 className='dosis'>{info.h1}</h1>
-                    <p className={info.quotes? 'quotes' : ''} dangerouslySetInnerHTML={{ __html: info.p}}></p>
-                    <footer>{info.footer}</footer>
-                    {info.btn? <Link to={info.link} className="btn btn-secondary btn-sm" target='_blank'>{info.btn}</Link> : ''}
+                    <h1 className='dosis'>{h1}</h1>
+                    <p className={quotes? 'quotes' : ''} dangerouslySetInnerHTML={{ __html: p}}></p>
+                    <footer>{footer}</footer>
+                    {btn? <Link to={link} className="btn btn-secondary btn-sm" target='_blank'>{btn}</Link> : ''}
                 </blockquote>
             </div>
         </div>
