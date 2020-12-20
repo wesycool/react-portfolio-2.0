a4dfeeef739e6fd365bb4c75fae2f03d5fdd8538
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-12-20 04:03:31 -0500

    Moved mainpage index to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import MainPage from "./mainpage/index"
-import Dashboard from "./dashboard/index"
+// import Dashboard from "./dashboard/index"
+import axios from "axios"
+
+import NavBar from "./mainpage/NavBar"
+import Footer from "./mainpage/Footer"
+import Home from "./mainpage/Home"
+import About from "./mainpage/About"
+import Contact from "./mainpage/Contact"
+
+import HeadContext from "./component/HeadContext"
+import PortfolioContext from "./component/PortfolioContext"
+import Portfolio from "./mainpage/Portfolio"
+import Project from "./mainpage/Project"
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Route path="/" component={MainPage}/>
-        <Route exact path="/dashboard" component={Dashboard}/>
-      </div>
-    </Router>
+  // return (
+  //   <Router>
+  //     <div>
+  //       <Route path="/" component={MainPage}/>
+  //       <Route exact path="/dashboard" component={Dashboard}/>
+  //     </div>
+  //   </Router>
 
-  );
+  // );
+
+  const [projects, setProjects] = useState([])
+    const [headTitle, setHeadTitle] = useState()
+    const mainHeadDescription = "Hi, I'm Edmund! I'm a Business Process Specialist from Toronto."
+    const [headDescription, setHeadDescription] = useState(mainHeadDescription)
+
+    useEffect( () => axios.get("/api/project-list").then( ({data}) => setProjects([...data].reverse())),[])
+    useEffect( () => {
+        document.title = `Edmund Wong${!headTitle? '' : ` | ${headTitle}`}`
+        document.querySelector('meta[name=description]').setAttribute('content',headDescription)
+    }, [headTitle,headDescription])
+
+    return (
+      <Router>
+        <div className="MainPage">
+            <NavBar/>
+            <div style={{marginTop:"3.875em"}}>
+                <div className="body">
+                    <HeadContext.Provider value={[setHeadTitle,setHeadDescription,mainHeadDescription]}>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/about" component={About}/>
+                        <Route exact path="/contact" component={Contact}/>
+                        
+                        <PortfolioContext.Provider value={projects}>
+                            <Route exact path="/portfolio" component={Portfolio}/>
+                            <Route path="/portfolio/:project" component={Project}/>
+                        </PortfolioContext.Provider>
+                    </HeadContext.Provider>
+                </div>
+
+                <Footer/>
+            </div>
+        </div>
+      </Router>
+    )
 }
 
 export default App;
