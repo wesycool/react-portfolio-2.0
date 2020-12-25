815159e27a5b8f854a4498c2afedc0a1ee16a583
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-12-25 15:47:43 -0500

    Update head and changed mainpage index

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect }from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import MainPage from "./mainpage/index"
-// import Dashboard from "./dashboard/index"
 import axios from "axios"
+import './App.css'
 
 import NavBar from "./mainpage/NavBar"
 import Footer from "./mainpage/Footer"
@@ -15,55 +14,42 @@ import PortfolioContext from "./component/PortfolioContext"
 import Portfolio from "./mainpage/Portfolio"
 import Project from "./mainpage/Project"
 
-import LifeCycle from './component/LifeCycle'
-
 function App() {
-  // return (
-  //   <Router>
-  //     <div>
-  //       <Route path="/" component={MainPage}/>
-  //       <Route exact path="/dashboard" component={Dashboard}/>
-  //     </div>
-  //   </Router>
-
-  // );
-
   const [projects, setProjects] = useState([])
-    const [headTitle, setHeadTitle] = useState()
-    const mainHeadDescription = "Hi, I'm Edmund! I'm a Business Process Specialist from Toronto."
-    const [headDescription, setHeadDescription] = useState(mainHeadDescription)
+  const [headTitle, setHeadTitle] = useState()
+  const mainHeadDescription = "Hi, I'm Edmund! I'm a Robotic Process Automation Business Analyst from Toronto."
+  const [headDescription, setHeadDescription] = useState(mainHeadDescription)
 
-    useEffect( () => axios.get("/api/project-list").then( ({data}) => setProjects([...data].reverse())),[])
-    useEffect( () => {
-        document.title = `Edmund Wong${!headTitle? '' : ` | ${headTitle}`}`
-        document.querySelector('meta[name=description]').setAttribute('content',headDescription)
-    }, [headTitle,headDescription])
+  useEffect( () => axios.get("/api/project-list").then( ({data}) => setProjects([...data].reverse())),[])
+  useEffect( () => {
+      document.title = `Edmund Wong${!headTitle? '' : ` | ${headTitle}`}`
+      document.querySelector('meta[name=description]').setAttribute('content',headDescription)
+  }, [headTitle,headDescription])
 
-    return (
-      
-      <Router>
-        <LifeCycle></LifeCycle>
-        <div className="MainPage">
-            <NavBar/>
-            <div style={{marginTop:"3.875em"}}>
-                <div className="body">
-                    <HeadContext.Provider value={[setHeadTitle,setHeadDescription,mainHeadDescription]}>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/about" component={About}/>
-                        <Route exact path="/contact" component={Contact}/>
-                        
-                        <PortfolioContext.Provider value={projects}>
-                            <Route exact path="/portfolio" component={Portfolio}/>
-                            <Route path="/portfolio/:project" component={Project}/>
-                        </PortfolioContext.Provider>
-                    </HeadContext.Provider>
-                </div>
 
-                <Footer/>
-            </div>
+  return (
+    <Router>
+      <div className="MainPage">
+        <NavBar/>
+        <div style={{marginTop:"3.875em"}}>
+          <div className="body">
+            <HeadContext.Provider value={[setHeadTitle,setHeadDescription,mainHeadDescription]}>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/contact" component={Contact}/>
+              
+              <PortfolioContext.Provider value={projects}>
+                <Route exact path="/portfolio" component={Portfolio}/>
+                <Route path="/portfolio/:project" component={Project}/>
+              </PortfolioContext.Provider>
+            </HeadContext.Provider>
+          </div>
+
+          <Footer/>
         </div>
-      </Router>
-    )
+      </div>
+    </Router>
+  )
 }
 
 export default App;
